d4f033d685561db8356f22cd05f31f55fddb4806
zalmaaroole <salmamaxamed274@gmail.com>
2025-06-20 15:34:21 +0300

    Second commit

---
+++ src/main/java/com/example/C1221346_60/C122134660Application.java
@@ -0,0 +1,13 @@
+package com.example.C1221346_60;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C122134660Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C122134660Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/C1221346_60/C122134660ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.C1221346_60;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C122134660ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
